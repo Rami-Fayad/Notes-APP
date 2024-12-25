edc92d5b33b7903e3122d6f2c9a7473737f7554b
Rami-Fayad <52130327@students.liu.edu.lb>
2024-12-25 14:35:30 +0200

    Login UI/User Input Validation  Completed

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,47 +1,68 @@
-import React, { useState } from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
-import InputPassword from '../../components/Inputpassword/InputPassword'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import InputPassword from "../../components/Inputpassword/InputPassword";
+import { validateEmail } from "../../utils/validate";
 
 const Login = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
-  const [error , seterror] =useState(null);
-  const handleLogin= async (e) => {
-  e.preventDefualt();
-  
-  }
+  const [error, seterror] = useState(null);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      seterror("Please Enter a valid email address");
+      return;
+    }
+    if (!password) {
+      seterror("Please Enter a Password");
+      return;
+    }
+    seterror("");
+
+    // Login API Call 
+  };
   return (
     <div>
-      <Navbar/>
-      <div className='flex items-center justify-center mt-28'>
-        <div className='w-96 border rounded bg-white py-10 px-7'>
-        <form onSubmit={handleLogin}>
-        <h4 className='text-2xl mb-7'>Login</h4>
-       <input type="text" placeholder='Enter your Email' className='input-box'
-       value={email} 
-       onChange={(e)=> {
-        setemail(e.target.value)
-       }}/>
-       <InputPassword
-       value={password}
-       onChange={(e)=>{
-        setpassword(e.target.value)
-       }}/>
-       <button type='submit' className='btn-primary'>
-        Login
-       </button>
-        <p className='text-sm text-center mt-4'>
-          Not registered yet? {""}
-          <Link to={'/signup'} className='font-medium text-primary underline'>
-          Create an account
-          </Link>
-        </p>
-      </form>
-      </div>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white py-10 px-7">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input
+              type="text"
+              placeholder="Enter your Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <InputPassword
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not registered yet? {""}
+              <Link
+                to={"/signup"}
+                className="font-medium text-primary underline"
+              >
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
