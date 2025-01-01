c36e1fb4eec8096ab2fcbfc37ce650a5df5a1837
Rami-Fayad <52130327@students.liu.edu.lb>
2025-01-01 19:03:44 +0200

    Note Card Hover Effect

--- Frontend/src/components/Cards/Notecard.jsx
+++ Frontend/src/components/Cards/Notecard.jsx
@@ -11,7 +11,7 @@ const Notecard = ({
   OnPin,
 }) => {
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <div className="flex items-center justify-between">
         <div>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
