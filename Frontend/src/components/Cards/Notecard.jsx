f231a329c3a3411d06cacbea639a0ac2ab94b5d3
Rami-Fayad <52130327@students.liu.edu.lb>
2025-01-01 18:58:12 +0200

    Note Card

---
+++ Frontend/src/components/Cards/Notecard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { MdCreate, MdDelete, MdOutlinePushPin } from "react-icons/md";
+const Notecard = ({
+  title,
+  date,
+  content,
+  tags,
+  ispinned,
+  onEdit,
+  onDelete,
+  OnPin,
+}) => {
+  return (
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex items-center justify-between">
+        <div>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+          <span className="text-sm text-slate-500">{date}</span>
+        </div>
+        <MdOutlinePushPin
+          size={20}
+          className={`icon-btn ${ ispinned ? "text-sm text-gray-500 dark:text-gray-300 hover:text-yellow-500 dark:hover:text-yellow-300": "text-slate-300 dark:text-slate-600"  } `}
+          onClick={OnPin}
+        />
+      </div>
+      <p class="mb-3 mt-2 font-normal text-gray-700 dark:text-gray-400">
+        {content?.slice(0, 60)}
+      </p>
+      <div class="flex flex-wrap mt-2 -mx-1">
+        <span className="px-2 py-1 mb-1 mr-1 text-xs font-medium text-blue-800 bg-blue-100 rounded-sm dark:bg-blue-200 dark:text-blue-900">
+          {tags}
+        </span>
+      </div>
+      <div className="flex items-center mt-4">
+        <MdCreate
+          size={20}
+          className="icon-btn text-sm text-gray-500 dark:text-gray-300 hover:text-green-500 dark:hover:text-gray-100"
+          onClick={onEdit}
+        />
+
+        <MdDelete
+          size={20}
+          className="icon-btn ml-3 text-sm text-red-500 dark:text-red-300 hover:text-red-600 dark:hover:text-red-100"
+          onClick={onDelete}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Notecard;
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Notecard from '../../components/Cards/Notecard'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <div className='container mx-auto'>
+      <div className='grid grid-cols-1 gap-3 mt-8 md:grid-cols-2 lg:grid-cols-3'>
+      <Notecard
+      title="Meeting on New Year"
+      date="12/12/2025"
+      content="Meeting with the team to discuss the new year plans."
+      tags="#Meeting"
+      ispinned={true}
+      onEdit={()=>{}}
+      OnDelete={()=>{}}
+      onPin={()=>{}}
+
+      />
+      <Notecard
+      title="Meeting on New Year"
+      date="12/12/2025"
+      content="Meeting with the team to discuss the new year plans."
+      tags="#Meeting"
+      ispinned={true}
+      onEdit={()=>{}}
+      OnDelete={()=>{}}
+      onPin={()=>{}}
+
+      />
+      <Notecard
+      title="Meeting on New Year"
+      date="12/12/2025"
+      content="Meeting with the team to discuss the new year plans."
+      tags="#Meeting"
+      ispinned={true}
+      onEdit={()=>{}}
+      OnDelete={()=>{}}
+      onPin={()=>{}}
+
+      />
+      <Notecard
+      title="Meeting on New Year"
+      date="12/12/2025"
+      content="Meeting with the team to discuss the new year plans."
+      tags="#Meeting"
+      ispinned={true}
+      onEdit={()=>{}}
+      OnDelete={()=>{}}
+      onPin={()=>{}}
+
+      />
+      </div>
+      </div>
+
     </>
   )
 }
